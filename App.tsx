8c6d7d2585557f30ef043eaeccb7ac94705199d2
Joel <98joefar@skola.boras.se>
2024-09-30 11:04:14 +0200

    !

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Henke är bök!</Text>
+      <Text>Henke är bök!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
